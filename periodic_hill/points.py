dbbb12b1a73a1da2819f73a7ac175df56714dcb0
mrkearden@gmail.com <mrkearden@gmail.com>
2020-04-11 04:14:41 -0400

    add periodic hill

---
+++ periodic_hill/points.py
@@ -0,0 +1,22 @@
+# --
+f = open("/home/titan/fea/turbulent/periodic_hill/geometry.csv","r")
+# Point Coordinates
+
+import math
+import salome
+salome.salome_init()
+import GEOM
+from salome.geom import geomBuilder
+geompy = geomBuilder.New(salome.myStudy)
+
+n=0
+for l in f:
+    x, y, z = [ float(v) for v in l.split() ]
+    pt = geompy.MakeVertex(x, y, z)
+    geompy.addToStudy(pt, "Pt_%s"%(n))
+    n += 1
+pass
+import salome
+salome.sg.updateObjBrowser()
+
+
